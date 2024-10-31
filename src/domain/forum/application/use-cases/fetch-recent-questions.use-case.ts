10e537d6ee28e1b97a48b55381e4c49eec2a360f
Luan Oliveira <luanoliveira17@hotmail.com>
2024-10-31 13:07:46 -0300

    feat: add question presenter

--- src/domain/forum/application/use-cases/fetch-recent-questions.use-case.ts
+++ src/domain/forum/application/use-cases/fetch-recent-questions.use-case.ts
@@ -1,6 +1,7 @@
 import { Either, right } from '@/core/either'
 import { Question } from '../../enterprise/entities/question.entity'
 import { QuestionsRepository } from '../repositories/questions.repository.interface'
+import { Injectable } from '@nestjs/common'
 
 interface FetchRecentQuestionsUseCaseRequest {
   page: number
@@ -13,6 +14,7 @@ type FetchRecentQuestionsUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class FetchRecentQuestionsUseCase {
   constructor(private readonly questionsRepository: QuestionsRepository) {}
 
