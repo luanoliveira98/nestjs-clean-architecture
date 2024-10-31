1f40f85fd9ad09e1b3af1454a395d9173181d7f4
Luan Oliveira <luanoliveira17@hotmail.com>
2024-10-31 12:42:00 -0300

    build: add asnwers table to database schema

--- src/domain/forum/enterprise/entities/question.entity.ts
+++ src/domain/forum/enterprise/entities/question.entity.ts
@@ -8,7 +8,7 @@ import { QuestionBestAnswerChosenEvent } from '../../events/question-best-answer
 
 export interface QuestionProps {
   authorId: UniqueEntityID
-  bestAnswerId?: UniqueEntityID
+  bestAnswerId?: UniqueEntityID | null
   title: string
   content: string
   slug: Slug
@@ -75,15 +75,8 @@ export class Question extends AggregateRoot<QuestionProps> {
     this.touch()
   }
 
-  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
-    if (bestAnswerId === undefined) {
-      return
-    }
-
-    if (
-      this.props.bestAnswerId === undefined ||
-      !this.props.bestAnswerId.equals(bestAnswerId)
-    ) {
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined | null) {
+    if (bestAnswerId && bestAnswerId !== this.props.bestAnswerId) {
       this.addDomainEvent(new QuestionBestAnswerChosenEvent(this, bestAnswerId))
     }
 
