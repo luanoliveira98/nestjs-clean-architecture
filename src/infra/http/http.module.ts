7b817d631461f70ee6087ceee2412d25d630ff94
Luan Oliveira <luanoliveira17@hotmail.com>
2024-11-04 13:01:35 -0300

    feat: add delete question controller

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -13,6 +13,8 @@ import { GetQuestionBySlugController } from './controllers/get-question-by-slug.
 import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug.use-case'
 import { EditQuestionController } from './controllers/edit-question.controller'
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question.use-case'
+import { DeleteQuestionController } from './controllers/delete-question.controller'
+import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question.use-case'
 @Module({
   imports: [DatabaseModule, CryptographyModule],
   controllers: [
@@ -22,6 +24,7 @@ import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-q
     FetchRecentQuestionController,
     GetQuestionBySlugController,
     EditQuestionController,
+    DeleteQuestionController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -30,6 +33,7 @@ import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-q
     RegisterStudentUseCase,
     GetQuestionBySlugUseCase,
     EditQuestionUseCase,
+    DeleteQuestionUseCase,
   ],
 })
 export class HttpModule {}
